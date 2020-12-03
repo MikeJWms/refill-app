1ac405c479e60ca3f624b0ea07357c2a4b4b0949
Michael Williams <michaeljwms@live.com>
2020-12-02 17:23:05 -0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      Launch
+      <b-icon
+        icon="rocket"
+        size="is-large"
+      />
+    </h2>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
